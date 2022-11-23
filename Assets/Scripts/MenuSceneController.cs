6fed083ad74239f43d4b405fa48da7b8a1415f16
ChangChi <sam54537@gmail.com>
2022-11-23 23:36:57 +0800

    feat: import fonts and basic menu scene

---
+++ Assets/Scripts/MenuSceneController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSceneController : MonoBehaviour
+{
+    public void OnStart()
+    {
+        Debug.Log("OnStart");
+    }
+
+    public void OnOption()
+    {
+        Debug.Log("OnOption");
+    }
+}
