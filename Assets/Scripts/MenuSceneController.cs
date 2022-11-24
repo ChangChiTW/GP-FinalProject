35c504f5d99ded06e627257403ea0d80b6118576
ChangChi <sam54537@gmail.com>
2022-11-24 14:19:14 +0800

    feat: background story and how to play

--- Assets/Scripts/MenuSceneController.cs
+++ Assets/Scripts/MenuSceneController.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class MenuSceneController : MonoBehaviour
 {
+    [SerializeField] private GameObject _backgroundStory;
+    [SerializeField] private GameObject _howToPlay;
     public void OnStart()
     {
-        Debug.Log("OnStart");
+        _backgroundStory.SetActive(true);
+    }
+
+    public void OnBackgroundStoryContinue()
+    {
+        _backgroundStory.SetActive(false);
+        _howToPlay.SetActive(true);
+    }
+
+    public void OnHowToPlayContinue()
+    {
+        _howToPlay.SetActive(false);
     }
 
     public void OnOption()
