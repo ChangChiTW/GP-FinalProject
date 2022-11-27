77c7a30f8e76a93059899eddcac46031e8343014
ChangChi <sam54537@gmail.com>
2022-11-27 23:46:01 +0800

    feat: book scene

--- Assets/Scripts/BookController.cs
+++ Assets/Scripts/BookController.cs
@@ -3,48 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
-public class turnBook : MonoBehaviour
-{   
+public class BookController : MonoBehaviour
+{
     public Animator book;
     public Canvas intro;
     public Canvas tutor;
+
     // Start is called before the first frame update
     void Start()
     {
-        intro.enabled =true;
-        tutor.enabled =false;
+        intro.enabled = true;
+        tutor.enabled = false;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() { }
 
-    public void turnPage(bool turnRight){
-        if(turnRight){
+    public void turnPage(bool turnRight)
+    {
+        if (turnRight)
+        {
             StartCoroutine(turnTutor());
-        }else{
+        }
+        else
+        {
             StartCoroutine(turnIntro());
         }
     }
-    public IEnumerator turnTutor(){
-        intro.enabled =false;
+
+    public IEnumerator turnTutor()
+    {
+        intro.enabled = false;
         book.SetTrigger("right");
         yield return new WaitForSeconds(0.8f);
-        tutor.enabled =true;
+        tutor.enabled = true;
     }
-    public IEnumerator turnIntro(){
-        tutor.enabled =false;
+
+    public IEnumerator turnIntro()
+    {
+        tutor.enabled = false;
         book.SetTrigger("left");
         yield return new WaitForSeconds(0.8f);
-        intro.enabled =true;
-        
+        intro.enabled = true;
     }
 
-    public void start(){
+    public void start()
+    {
         SceneManager.LoadScene("scenename");
     }
-
 }
