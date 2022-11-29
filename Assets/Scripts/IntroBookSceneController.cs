7f03c12c6792f1a0ff57d391c0949c3523b8be8a
ChangChi <sam54537@gmail.com>
2022-11-29 16:37:41 +0800

    feat: intro scene

--- Assets/Scripts/IntroBookSceneController.cs
+++ Assets/Scripts/IntroBookSceneController.cs
@@ -11,6 +11,10 @@ public class IntroBookSceneController : MonoBehaviour
     [SerializeField]
     private GameObject _inPageUI;
 
+    [SerializeField]
+    private GameObject[] _pageContents = new GameObject[2];
+    private int _currentPage = 0;
+
     void Start()
     {
         StartCoroutine(openBook());
@@ -21,10 +25,13 @@ public class IntroBookSceneController : MonoBehaviour
         _introBook.GetComponent<BookAnimator>().OpenBook();
         yield return new WaitForSeconds(0.8f);
         _inPageUI.SetActive(true);
+        _pageContents[0].SetActive(true);
     }
 
     private IEnumerator closeBook()
     {
+        _pageContents[_currentPage].SetActive(false);
+        _inPageUI.SetActive(false);
         _introBook.GetComponent<BookAnimator>().CloseBook();
         yield return new WaitForSeconds(0.8f);
         SceneManager.LoadScene("MainScene");
@@ -32,14 +39,30 @@ public class IntroBookSceneController : MonoBehaviour
 
     private IEnumerator flipRight()
     {
-        _introBook.GetComponent<BookAnimator>().FlipRight();
-        yield return new WaitForSeconds(0.8f);
+        if (_currentPage != 0)
+        {
+            _pageContents[_currentPage].SetActive(false);
+            _inPageUI.SetActive(false);
+            _introBook.GetComponent<BookAnimator>().FlipRight();
+            yield return new WaitForSeconds(0.6f);
+            _currentPage--;
+            _pageContents[_currentPage].SetActive(true);
+            _inPageUI.SetActive(true);
+        }
     }
 
     private IEnumerator flipLeft()
     {
-        _introBook.GetComponent<BookAnimator>().FlipLeft();
-        yield return new WaitForSeconds(0.8f);
+        if (_currentPage != _pageContents.Length - 1)
+        {
+            _pageContents[_currentPage].SetActive(false);
+            _inPageUI.SetActive(false);
+            _introBook.GetComponent<BookAnimator>().FlipLeft();
+            yield return new WaitForSeconds(0.6f);
+            _currentPage++;
+            _pageContents[_currentPage].SetActive(true);
+            _inPageUI.SetActive(true);
+        }
     }
 
     public void OnCloseBook()
