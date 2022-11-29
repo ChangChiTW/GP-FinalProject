c09fcfcfb806f36ccadc502a49a21711fc44196e
ChangChi <sam54537@gmail.com>
2022-11-29 13:33:19 +0800

    feat: switch two book

--- Assets/Scripts/MainSceneController.cs
+++ Assets/Scripts/MainSceneController.cs
@@ -6,21 +6,56 @@ using UnityEngine.SceneManagement;
 public class MainSceneController : MonoBehaviour
 {
     [SerializeField]
-    private GameObject _main;
+    private GameObject _introBook;
 
     [SerializeField]
-    private GameObject _npc1;
+    private GameObject _stageBook;
 
-    public void OnNpcInfo()
+    [SerializeField]
+    private GameObject _introBookmark;
+
+    [SerializeField]
+    private GameObject _stageBookmark;
+
+    private float _openBookmarkX;
+    private float _closeBookmarkX;
+
+    void Start()
     {
-        _npc1.SetActive(true);
-        _main.SetActive(false);
+        _openBookmarkX = _introBookmark.GetComponent<Transform>().position.x;
+        _closeBookmarkX = _stageBookmark.GetComponent<Transform>().position.x;
     }
 
-    public void OnBackToMainScene()
+    public void OnOpenIntroBook()
     {
-        _npc1.SetActive(false);
-        _main.SetActive(true);
+        _introBook.SetActive(true);
+        _introBookmark.GetComponent<Transform>().position = new Vector3(
+            _openBookmarkX,
+            _introBookmark.GetComponent<Transform>().position.y,
+            _introBookmark.GetComponent<Transform>().position.z
+        );
+        _stageBook.SetActive(false);
+        _stageBookmark.GetComponent<Transform>().position = new Vector3(
+            _closeBookmarkX,
+            _stageBookmark.GetComponent<Transform>().position.y,
+            _stageBookmark.GetComponent<Transform>().position.z
+        );
+    }
+
+    public void OnOpenStageBook()
+    {
+        _introBook.SetActive(false);
+        _introBookmark.GetComponent<Transform>().position = new Vector3(
+            _closeBookmarkX,
+            _introBookmark.GetComponent<Transform>().position.y,
+            _introBookmark.GetComponent<Transform>().position.z
+        );
+        _stageBook.SetActive(true);
+        _stageBookmark.GetComponent<Transform>().position = new Vector3(
+            _openBookmarkX,
+            _stageBookmark.GetComponent<Transform>().position.y,
+            _stageBookmark.GetComponent<Transform>().position.z
+        );
     }
 
     public void OnGoShop()
--- Assets/Scripts/MenuSceneController.cs
+++ Assets/Scripts/MenuSceneController.cs
@@ -5,26 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MenuSceneController : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject _backgroundStory;
-
-    [SerializeField]
-    private GameObject _howToPlay;
-
     public void OnStart()
     {
-        _backgroundStory.SetActive(true);
-    }
-
-    public void OnBackgroundStoryContinue()
-    {
-        _backgroundStory.SetActive(false);
-        _howToPlay.SetActive(true);
-    }
-
-    public void OnHowToPlayContinue()
-    {
-        _howToPlay.SetActive(false);
         SceneManager.LoadScene("MainScene");
     }
 
