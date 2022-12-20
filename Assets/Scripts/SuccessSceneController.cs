dd797c99bc2c80ceba56760c114ecf80ffb0a2f7
ChangChi <sam54537@gmail.com>
2022-12-20 18:57:34 +0800

    feat: success scene

---
+++ Assets/Scripts/SuccessSceneController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SuccessSceneController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _balanceText;
+
+    private StateManager _stateManager;
+
+    void Awake()
+    {
+        _stateManager = GameObject.Find("StateManager").GetComponent<StateManager>();
+    }
+
+    void Start()
+    {
+        _balanceText.GetComponent<TMP_Text>().text = "$" + _stateManager.GetBalance().ToString();
+    }
+
+    public void OnNextBtnClick()
+    {
+        _stateManager.Reset();
+        SceneManager.LoadScene("MenuScene");
+    }
+}
