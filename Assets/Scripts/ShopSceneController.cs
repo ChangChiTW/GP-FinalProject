4b133a368ca5b0acb9c00be449dcdca32d3f2f08
ChangChi <sam54537@gmail.com>
2022-12-20 20:30:36 +0800

    refactor: shop scene play btn

--- Assets/Scripts/ShopSceneController.cs
+++ Assets/Scripts/ShopSceneController.cs
@@ -23,4 +23,10 @@ public class ShopSceneController : MonoBehaviour
         _stateManager.SetStageBookPage(0);
         SceneManager.LoadScene("StageBookScene");
     }
+
+    public void OnGoDungeon()
+    {
+        _stateManager.AddLayer();
+        SceneManager.LoadScene("DungeonRunScene");
+    }
 }
